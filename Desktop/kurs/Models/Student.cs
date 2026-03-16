04372e1344dfedcbed7c7b89712f692cd555bd1e
AnvarKabirli <kebirlienver@gmail.com>
2026-03-16 13:52:39 +0400

    class

---
+++ Desktop/kurs/Models/Student.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kurs.Models
+{
+    
+ 
+    class Student
+    {
+         string username;
+         string firstname;
+         string lastname; 
+         int groupnumber;
+        int birthday; 
+        string adress;
+         int phonenumber;
+         string email;
+         string pasword;
+         int score;
+
+        public Student()
+        {
+           
+        }
+
+        public Student(string name, string firstname, string lastname, int groupnumber,
+         int birthday,
+         string email,string pasword)
+        {
+            this.username = name;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.groupnumber = groupnumber;
+            this.birthday = birthday;
+            this.email = email;
+            this.pasword = pasword;
+
+        }
+
+        public Student(string name, string firstname, string lastname, 
+         string pasword,
+         string email)
+        {
+            this.username = name;
+            this.firstname = firstname;
+                this.lastname = lastname;
+            this.pasword = pasword;
+                this.email = email;
+            }
+
+
+        public Student(string name, string firstname, string lastname, int groupnumber,
+         int birthday,
+         string adess,
+         int phonenumber,
+         string email,
+        string pasword,
+         int score)
+        {
+            this.username = name;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.groupnumber = groupnumber;
+            this.birthday = birthday;
+            this.adress = adess;
+        this.phonenumber = phonenumber;
+        this.email = email;
+        this.pasword = pasword;
+        this.score = score;
+        }
+    }
+
+}
--- Desktop/kurs/Program.cs
+++ Desktop/kurs/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//oupnumber} birthday: {birthday} adress; {adress} phonenumber: {phonenumber} email:{email} pasword: {pasword} score: {score}              ");
+using kurs.Models;
+
+Student student = new Student();
+
+student("ali", "aliyev", "ali", "4", "22", "salam", "salam");
+Console.WriteLine(student);
+
+public void GetName("firstname" + "lastname")
+{
+
+}
