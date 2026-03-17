53a26641e78e335cf9d40657486c939b0df717c3
AnvarKabirli <kebirlienver@gmail.com>
2026-03-17 13:40:03 +0400

    register

--- Desktop/kurs/Models/Student.cs
+++ Desktop/kurs/Models/Student.cs
@@ -8,76 +8,76 @@ namespace kurs.Models
 {
     
  
-    class Student
+   public class Student
     {
-        public  string username;
-        public string firstname;
-        public string lastname;
-        public int groupnumber;
-        public string birthday;
-        public string adress;
-        public  int phonenumber;
-        public  string email;
-        public string pasword;
-        public int score;
+        public  string username { get; set; }
+        public string firstname{ get; set; }
+        public string lastname{ get; set; }
+        public int groupnumber{ get; set; }
+        public string birthday{ get; set; }
+        public string adress{ get; set; }
+        public  int phonenumber{ get; set; }
+        public  string email{ get; set; }
+        public string pasword{ get; set; }
+        public int score{ get; set; }
 
-        public Student()
-        {
-           
-        }
+        //public Student()
+        //{
 
-        public Student(string name, string firstname, string lastname, int groupnumber,
-         string birthday,
-         string email,string pasword)
-        {
-            this.username = name;
-            this.firstname = firstname;
-            this.lastname = lastname;
-            this.groupnumber = groupnumber;
-            this.birthday = birthday;
-            this.email = email;
-            this.pasword = pasword;
+        //}
 
-        }
+        //public Student(string name, string firstname, string lastname, int groupnumber,
+        // string birthday,
+        // string email,string pasword)
+        //{
+        //    this.username = name;
+        //    this.firstname = firstname;
+        //    this.lastname = lastname;
+        //    this.groupnumber = groupnumber;
+        //    this.birthday = birthday;
+        //    this.email = email;
+        //    this.pasword = pasword;
 
-        public Student(string name, string firstname, string lastname, 
-         string pasword,
-         string email)
-        {
-            this.username = name;
-            this.firstname = firstname;
-                this.lastname = lastname;
-            this.pasword = pasword;
-                this.email = email;
-            }
+        //}
+
+        //public Student(string name, string firstname, string lastname, 
+        // string pasword,
+        // string email)
+        //{
+        //    this.username = name;
+        //    this.firstname = firstname;
+        //        this.lastname = lastname;
+        //    this.pasword = pasword;
+        //        this.email = email;
+        //    }
 
 
-        public Student(string name, string firstname, string lastname, int groupnumber,
-         string birthday,
-         string adess,
-         int phonenumber,
-         string email,
-        string pasword,
-         int score)
-        {
-            this.username = name;
-            this.firstname = firstname;
-            this.lastname = lastname;
-            this.groupnumber = groupnumber;
-            this.birthday = birthday;
-            this.adress = adess;
-        this.phonenumber = phonenumber;
-        this.email = email;
-        this.pasword = pasword;
-        this.score = score;
-        }   
+        //public Student(string name, string firstname, string lastname, int groupnumber,
+        // string birthday,
+        // string adess,
+        // int phonenumber,
+        // string email,
+        //string pasword,
+        // int score)
+        //{
+        //    this.username = name;
+        //    this.firstname = firstname;
+        //    this.lastname = lastname;
+        //    this.groupnumber = groupnumber;
+        //    this.birthday = birthday;
+        //    this.adress = adess;
+        //this.phonenumber = phonenumber;
+        //this.email = email;
+        //this.pasword = pasword;
+        //this.score = score;
+        //}   
 
-        public void GetInfo()
-        {
-            Console.WriteLine($"username:{username} firstname: {firstname} lastname: {lastname} groupnumber:{groupnumber} birthday: {birthday}");
+        //public void GetInfo()
+        //{
+        //    Console.WriteLine($"username:{username} firstname: {firstname} lastname: {lastname} groupnumber:{groupnumber} birthday: {birthday}");
 
-        }
-            
+        //}
+
     }
 
 }
---
+++ Desktop/kurs/Register/RegisterStudent.cs
@@ -0,0 +1,24 @@
+﻿using kurs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using kurs.Models;
+public static class RegisterStudent
+{
+    static List<Student> students = new List<Student>();
+
+    public static void Register(Student s)
+    {
+        students.Add(s);
+
+        foreach (var item in students)
+        {
+            Console.WriteLine($"{item.firstname} {item.lastname}");
+        }
+        Console.WriteLine();
+    }
+}
+
